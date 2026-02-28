1695acbcc3b3c6bd30b37c28ac810abedcdb7c05
kkk <3352643415@qq.com>
2026-02-28 14:57:53 +0800

    Initial commit

---
+++ src/navigation/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kkk',
+    maintainer_email='kkk@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'offboard_control_takeoff_and_land = navigation.offboard_mode:main',
+            'offboard_cruise = navigation.offboard_cruise:main'
+
+        ],
+    },
+)
